3d4ca0cf919641d8736280c8e08b0feaf2119f68
edufalcao14 <eduardo.sampaio@student.vinci.be>
2024-10-28 23:11:29 +0100

    add : routes pages config

---
+++ vinci-hotel/app/finances/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Finances() {
+    return(
+        <div>
+            <h1>Faitez ici les dahsboard de finance</h1>
+        </div>
+    )
+}
---
+++ vinci-hotel/app/reservations/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Reservations() {
+    return(
+        <div>
+            <h1>Faitez ici les dahsboard de Reservations</h1>
+        </div>
+    )
+}

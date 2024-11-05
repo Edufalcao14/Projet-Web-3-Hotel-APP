0bb47497fe99d65e4e916e6bfe655481c253b73c
Koubai <omar.koubai@student.vinci.be>
2024-11-05 17:47:02 +0100

    1 version donut component

--- vinci-hotel/app/rooms/page.jsx
+++ vinci-hotel/app/rooms/page.jsx
@@ -1,9 +1,19 @@
-
+"use client"
+import DonutChart from "../../components/donutChart/index.jsx"; // Assurez-vous que le nom du fichier est correct
 
 export default function Rooms() {
-    return(
-        <div>
-            <h1>Faitez ici les dahsboard de Chambres</h1>
-        </div>
-    )
+  const donutChartData = [
+    { asset: "Stocks", amount: 60000 },
+    { asset: "Bonds", amount: 40000 },
+    { asset: "Cash", amount: 7000 },
+    { asset: "Real Estate", amount: 5000 },
+    { asset: "Commodities", amount: 3000 },
+  ];
+
+  return (
+      <div>
+        <h1>Rooms</h1>
+        <DonutChart data={donutChartData} title="Portfolio Distribution" />
+      </div>
+  );
 }
